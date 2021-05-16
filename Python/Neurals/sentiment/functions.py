7916b37ed4cbf93214492fb71278659add7db315
Vojtěch Smutný <vutristo@gmail.com>
2021-05-16 09:02:28 +0200

    add input, id, tokens

--- Python/Neurals/sentiment/functions.py
+++ Python/Neurals/sentiment/functions.py
@@ -1,11 +1,16 @@
 import os
+import re
+
 
 def remove_empty_lists(list_data):
    return [ele for ele in list_data if ele != []]
 
+
 def get_subdir_name_alt(relative_path):
     dirs = [folder[1] for folder in os.walk(relative_path)]
+
     return remove_empty_lists(dirs)
+
 
 def get_subdir_names_and_files(relative_path):
     subdir_names = []
@@ -21,25 +26,53 @@ def get_subdir_names_and_files(relative_path):
     
     return subdir_file, subdir_names
 
-def get_dataframe(folder):
 
-    file_info = []
+def get_dataframe(folder):
+    dataframe = []
 
     for filename in os.listdir(folder):
         file = {}
-        file["file_path"] = filename
+        file["folder_name"] = folder
+        file["file_name"] = filename
+
         with open(os.path.join(folder, filename), 'r') as f:
             file["plain_text"] = f.read()
-            file_info.append(file)
+            dataframe.append(file)
             
-    
-    return file_info
+    return dataframe
 
 
-def get_dataframe_from_folders(rel_path_list):
+def get_dataframe_in_subfolders(rel_path_list):
     dataframe = []
 
     for folder in rel_path_list:
         dataframe.append(get_dataframe(folder))
     
+    return dataframe
+
+def get_tokenized_list(plain_text):
+    word = []
+    word = re.split('\W+', plain_text.lower())
+    word = list(filter(None, word))
+
+    return word
+
+def get_target_value(folder_name):
+    if folder_name == "dataset/BAD":
+        return 1
+    elif folder_name == "dataset/GOOD":
+        return 2
+    else:
+        return 3
+
+def del_filename_extension(filename, extension):
+    return re.sub(extension, "", filename)
+
+def get_tokens_id_input_to_dataframe(dataframe, plain_text_key, folder_key, filename_key):
+    for data_block in dataframe:
+        for data in data_block:
+            data["tokenized_text"] = get_tokenized_list(data[plain_text_key])
+            data["input"] = get_target_value(data[folder_key])
+            data["text_id"] = del_filename_extension(data[filename_key], ".txt")
+
     return dataframe
--- Python/Neurals/sentiment/sentiment.py
+++ Python/Neurals/sentiment/sentiment.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import os
+import re
 from functions import *
 
-dataset_relative_path = "dataset"
 
 
 
@@ -14,13 +14,23 @@ Required structure:
 1, 2, 3: BAD, GOOD, NEUTRAL
 
 {
+    file_name: 9.txt
+    plain_text: But it does n't leave you with much .
+    folder_name: dataset/BAD
+
     text_id: 9
-    text: But it does n't leave you with much .
+    tokenized_text: ["but", "it", "doesnt", "leave", "you", "with", "much"]
     input: 1
 }
 
-"""
-subdir_paths = get_subdir_names_and_files(dataset_relative_path)[0]
-dataframe = get_dataframe_from_folders(subdir_paths)
 
+"""
+dataset_relative_path = "dataset"
+subdir_paths = get_subdir_names_and_files(dataset_relative_path)[0] #[1] would get txt file names
+
+dataframe = get_dataframe_in_subfolders(subdir_paths)
+dataframe = get_tokens_id_input_to_dataframe(dataframe, "plain_text", "folder_name", "file_name")
 print(dataframe)
+
+# ADD ID
+# ADD INPUT (1, 2, 3)
